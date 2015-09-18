47dcf55f5d44ad2c581d3da570b7b250187985ac
wanglei <wanglei@allinpay.com>
2015-09-18 11:01:36 +0800

    initial.

---
+++ src/test/java/com/allinpay/sample/multithread/threadpool/NewFixedThreadPoolTest.java
@@ -0,0 +1,25 @@
+package com.allinpay.sample.multithread.threadpool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class NewFixedThreadPoolTest {
+	public static void main(String[] args) {
+		ExecutorService executorService = Executors.newFixedThreadPool(10);
+		int i = 0;
+		while (i < 5) {
+			executorService.execute(new Runnable() {
+				public void run() {
+					try {
+						Thread.sleep(10 * 1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					System.out.println(".................");
+				}
+			});
+			i++;
+		}
+	}
+
+}
